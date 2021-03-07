53b97e05000ccce8399be1ee4e93f0f1dafd45fb
Gabriel Tupy <gabetupy@gmail.com>
2021-03-07 13:22:08 -0600

    Started Setting Up File Structure and Create a Main Menu Scene to get us started

---
+++ Assets/Scripts/Components/SceneChanger.cs
@@ -0,0 +1,21 @@
+﻿//Written By Gabriel Tupy 3-7-2021
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    /// <summary>
+    /// The first Arguement is the previous scene and the second is the new scene being changed to.
+    /// </summary>
+    public static Action<Scene, Scene> OnSceneChange;
+
+    public void ChangeScene(string nextScene)
+    {
+        OnSceneChange?.Invoke(SceneManager.GetActiveScene(), SceneManager.GetSceneByName(nextScene));
+        SceneManager.LoadScene(nextScene);
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(nextScene));
+    }
+}
